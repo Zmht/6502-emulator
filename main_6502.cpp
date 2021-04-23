2c4cf557e88d2e12aad3b2a11c91a1e7b9f5577f
Zach Hansen Terry <10217473@LPSC02TC0XDGFWM.local>
2021-04-23 15:58:46 -0500

    added a reset function

--- main_6502.cpp
+++ main_6502.cpp
@@ -2,7 +2,8 @@
  **************************************************************************************************************************
  * Zachary Hansen Terry: zmht25@gmail.com * https://zmht25.ddns.net/                                                      *
  *                                                                                                                        *
- * This is the 6502 emulator. I made it by watching a tutorial on youtube: https://www.youtube.com/watch?v=qJgsuQoy9bc    *
+ * This is the 6502 emulator. I made it by watching a tutorial on youtube: https://www.youtube.com/watch?v=qJgsuQoy9bc 
+ * Also I used this: http://www.obelisk.me.uk/6502/ resource a lot.   *
  **************************************************************************************************************************
 */
 
@@ -10,14 +11,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// The CPU class. Has Byte and Word data types. Has Registers, Program Counter, Stack Pointer.
 struct CPU
 {
     using Byte = unsigned char; //A unsigned char is 8 bits
     using Word = unsigned short; //A unsigned char is 16 bits. Perfec!
 
-    Word PC;
-    Word SP;
+    Word PC; // The program counter tells you what the next program to run is (16 bit)
+    Word SP; // The stack pointer is a 16 bit piece of information that tells you where you were on the 
     
+    Byte A, X, Y; //A, X, and Y registers.
+
+    // These are the status flags
+    Byte C : 1; // Carry flag.
+    Byte Z : 1; // Zero Flag
+    Byte I : 1; // Interrupt Disable
+    Byte D : 1; // Decimal Bit
+    Byte B : 1; // Break Command
+    Byte V : 1; // Overflow Flag
+    Byte N : 1; // Negative Flag
+
+    void reset()
+    {
+        PC = 0xFFFC; // Sets the program counter to $FFFC.
+        SP = 0x0100; // Sets the Stack Pointer to $0x0100.
+        D = 0;
+
+        A = 0;
+        X = 0;
+        Y = 0;
+    }
+
 };
 
 
@@ -25,5 +49,7 @@ struct CPU
 
 int main()
 {
-
+    CPU cpu;
+    cpu.reset();
+    return 0;
 }
