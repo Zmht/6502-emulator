a05589854962a22866320a1d3e49c27c5095a820
Zach Hansen Terry <10217473@LPSC02TC0XDGFWM.local>
2021-04-23 13:42:06 -0500

    added header comment

--- main_6502.cpp
+++ main_6502.cpp
@@ -1,16 +1,23 @@
+/*
+ **************************************************************************************************************************
+ * Zachary Hansen Terry: zmht25@gmail.com * https://zmht25.ddns.net/                                                      *
+ *                                                                                                                        *
+ * This is the 6502 emulator. I made it by watching a tutorial on youtube: https://www.youtube.com/watch?v=qJgsuQoy9bc    *
+ **************************************************************************************************************************
+*/
+
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 
 struct CPU
 {
-    using Byte = unsigned char;
-    using Word = unsigned short;
+    using Byte = unsigned char; //A unsigned char is 8 bits
+    using Word = unsigned short; //A unsigned char is 16 bits. Perfec!
 
     Word PC;
     Word SP;
     
-
 };
 
 
